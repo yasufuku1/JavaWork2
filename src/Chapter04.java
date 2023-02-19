c1d0649755c43cfb0d9b8637601c7e5bbf588e6f
yasusu_ 500 <eriyasu@gmail.com>
2023-02-19 17:07:39 +0900

    [Finish]java_chapter05

---
+++ src/Chapter04.java
@@ -0,0 +1,11 @@
+
+public class Chapter04 {
+	public static void main(String[] args) {
+	String str1 = "name";
+	System.out.println(str1);
+	str1 ="like";
+	System.out.println(str1);
+	final int LUCKY_NUMBER = 8;
+	System.out.println(LUCKY_NUMBER);
+	}
+}
---
+++ src/Chapter05.java
@@ -0,0 +1,19 @@
+
+public class Chapter05 {
+	public static void main(String[] args) {
+		long num1 = 1L;
+		double num2 = 0.1;
+		boolean boolean1 = true;
+		System.out.println(num1);
+		System.out.println(num2);
+		System.out.println(boolean1);
+		Float float1 = 2.0f;
+		Integer integer1 = 5;
+		Character chara1 = 'こ'; //Character」は、「``」で囲まれた１文字で初期化
+		System.out.println(float1);
+		System.out.println(integer1);
+		System.out.println(chara1);
+		int[] array1 = new int[] {10, 20, 50 };
+		System.out.println(array1[2]);
+	}
+}
