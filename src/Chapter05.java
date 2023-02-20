dbccfa75d36bb9005da48b14dfee3f38eac88f06
yasusu_ 500 <eriyasu@gmail.com>
2023-02-20 17:22:38 +0900

    [Finish]chapter6

--- src/Chapter05.java
+++ src/Chapter05.java
@@ -1,19 +1,22 @@
 
 public class Chapter05 {
 	public static void main(String[] args) {
-		long num1 = 1L;
+		long num1 = 1000000000000L;
 		double num2 = 0.1;
 		boolean boolean1 = true;
+
 		System.out.println(num1);
 		System.out.println(num2);
 		System.out.println(boolean1);
-		Float float1 = 2.0f;
-		Integer integer1 = 5;
+
+		Float float1 = 2.515f;
+		Integer integer1 = 500;
 		Character chara1 = 'こ'; //Character」は、「``」で囲まれた１文字で初期化
 		System.out.println(float1);
 		System.out.println(integer1);
 		System.out.println(chara1);
-		int[] array1 = new int[] {10, 20, 50 };
-		System.out.println(array1[2]);
+
+		int[] array1 = new int[] { 10, 20, 30, 40, 50 };
+		System.out.println(array1[3]);
 	}
 }
